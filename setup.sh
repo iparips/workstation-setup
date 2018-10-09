b99ec7d14aa17900f248811fedcb765eeae07943
Ilya Paripsa <ilya.paripsa@rea-group.com>
2018-10-09 16:18:17 +1100

    Remove unused dependencies, add missing tools

--- setup.sh
+++ setup.sh
@@ -13,8 +13,10 @@ if [ ! -f "${brew_executable}" ]; then
   brew update
 fi
 
-# Install git
-brew install git
+brew install git \
+ z nvm chruby thefuck \
+ bash-git-prompt ag jq \
+ bash-completion
 
 # Install ruby
 if [ ! -d "${ruby_dir}" ]; then
@@ -26,28 +28,10 @@ if [ ! -d "${ruby_dir}" ]; then
   ruby-install ruby # installs latest stable ruby
 fi
 
-# Install z
-brew install z
-
 # Link bash_profile & prompt
 ln -F -s ${setup_root}/shell/.bash_profile ~/.bash_profile
 ln -F -s ${setup_root}/shell/.prompt ~/.prompt
 source ~/.bash_profile
-
-# Install withenv
-rm -rf ${setup_root}/tmp
-mkdir ${setup_root}/tmp
-
-if [ ! -d "${tools_dir}/withenv-master" ]; then
-
-  if [ ! -d "${tools_dir}" ]; then
-    mkdir ${tools_dir}
-  fi
-
-  git clone git@github.com:mdub/withenv.git ${setup_root}/tmp/withenv-master
-  mv -f ${setup_root}/tmp/withenv-master /usr/local/tools/
-
-fi
 
 # Cleanup
 rm -rf ${setup_root}/tmp
