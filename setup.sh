35573c210ebbe2eb821498dba24f889900a50063
Ilya Paripsa <iparips@gmail.com>
2018-12-04 11:24:58 +1100

    Make setup relevant for NAB
    - assumes that brew is installed separately by hand
    - ruby is not required

--- setup.sh
+++ setup.sh
@@ -3,30 +3,11 @@
 set -e # -x # break on error, verbose
 
 setup_root=`pwd`
-tools_dir=/usr/local/tools
-brew_executable=/usr/local/bin/brew
-ruby_dir=~/.rubies/ruby-2.1.2
-
-# Install homebrew
-if [ ! -f "${brew_executable}" ]; then
-  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-  brew update
-fi
 
 brew install git \
- z nvm chruby thefuck \
+ z nvm \
  bash-git-prompt ag jq \
  bash-completion
-
-# Install ruby
-if [ ! -d "${ruby_dir}" ]; then
-  brew install chruby
-  brew install ruby-install
-
-  ## Install ruby pre-requisites
-  brew install openssl readline libyaml gdbm libffi
-  ruby-install ruby # installs latest stable ruby
-fi
 
 # Link bash_profile & prompt
 ln -F -s ${setup_root}/shell/.bash_profile ~/.bash_profile
